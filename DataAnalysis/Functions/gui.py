c6af540083037779e78e96fb61e9b0e3e3b27662
Archisa Kar <ak2023@hw.ac.uk>
2023-12-04 21:09:05 +0400

    added file explorer often to choose json file

--- DataAnalysis/Functions/gui.py
+++ DataAnalysis/Functions/gui.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from .cw_requirements import read_file, view_broswer, views_country
 from .graphs import *
+from tkinter import filedialog
 
 '''
 GUI for the program 
@@ -417,9 +418,27 @@ class DocOverview(tk.Frame):
         else:
             doc_overview_graph(documents, doc_uuid)
         
-def startGUI():
-    app = DataVisualise()
-    global file_path
-    file_path = 'Dataset/sample_small.json'
+# def startGUI():
+#     app = DataVisualise()
+#     global file_path
+#     file_path = 'Dataset/sample_small.json'
 
+#     app.mainloop()
+
+def get_file_path():
+    file_path = filedialog.askopenfilename(title="Select a JSON file", filetypes=[("JSON files", "*.json")])
+    return file_path
+
+def startGUI():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main Tkinter window
+
+    global file_path
+    file_path = get_file_path()
+    
+    if not file_path:
+        # User canceled the file selection
+        return
+
+    app = DataVisualise()
     app.mainloop()
